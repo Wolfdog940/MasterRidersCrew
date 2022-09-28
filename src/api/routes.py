49a358fb2438cd6d47d56de33b7519aceccd1c4f
Alejandrinaflores <floresyunny@gmail.com>
2022-09-28 21:16:47 +0000

    fixing conflict

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from sqlalchemy.sql import text
+from flask_jwt_extended import (
+    JWTManager, jwt_required, get_jwt_identity,
+    create_access_token,get_jwt
+)
 
 
 api = Blueprint('api', __name__)
@@ -17,6 +22,7 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
 
 @api.route("/signup", methods=["POST"])
 def register():
@@ -40,3 +46,16 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return jsonify(new_user.serialize()), 200
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({"token": access_token, "user_id": user.id}), 200
