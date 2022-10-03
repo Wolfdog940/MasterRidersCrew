671a3c91aee390e08535e4e1484f030815279126
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-10-03 15:50:16 +0000

    login ok

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -22,59 +22,49 @@ const signUp = () => {
   }
 
   return (
-    <section>
-      <div className="container mt-5 pt-5">
-        <div className="row justify-content-center">
-          <div className="col-md-6 text-center mb-2"></div>
+    <section className="d-flex justify-content-center align-items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="d-flex flex-column align-items-center"
+      >
+        <h3 className="text-white my-3">Sign Up</h3>
+        <div className="form-group d-flex justify-content-center">
+          <input
+            onChange={handleInputChange}
+            type="email"
+            className="form-control rounded-pill bg-transparent my-2 text-center text-white"
+            placeholder="email"
+            required=""
+          />
         </div>
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <div className="login-wrap p-0">
-              <h3 className="registro mb-5 text-center pb-2 pt-5 mt-1 ">
-                Registro
-              </h3>
-              <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <input
-                    onChange={handleInputChange}
-                    type="email"
-                    className="form-control border rounded-pill px-6 bg-transparent mt-2 text-center input"
-                    placeholder="email"
-                    required=""
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    onChange={handleInputChange}
-                    type="password"
-                    className="form-control border rounded-pill px-6 bg-transparent mt-2 mb-2 text-center input"
-                    placeholder="Password"
-                    required=""
-                  />
-                </div>
+        <div className="form-group d-flex justify-content-center">
+          <input
+            onChange={handleInputChange}
+            type="password"
+            className="form-control rounded-pill bg-transparent my-2 text-center text-white"
+            placeholder="Password"
+            required=""
+          />
+        </div>
 
-                <div className="form-group">
-                  <Link to="/">
-                    <button
-                      type="submit"
-                      className="form-control btn btn-light submit border rounded-pill px-6"
-                    >
-                      Registrate
-                    </button>
-                  </Link>
-                </div>
-                <div className="form-group d-md-flex d-flex justify-content-center">
-                  <div className="w-50 text-md-right  mt-2 fs-5 fw-bolder text-center">
-                    <Link to="/">
-                      <span className="text-white">Ya estas registrado?</span>
-                    </Link>
-                  </div>
-                </div>
-              </form>
-            </div>
+        <div className="form-group w-50 my-2">
+          <Link to="/">
+            <button
+              type="submit"
+              className="form-control btn btn-light submit rounded-pill bg-transparent text-white w-100"
+            >
+              Registrate
+            </button>
+          </Link>
+        </div>
+        <div className="form-group d-flex justify-content-center">
+          <div className="mt-3">
+            <Link to="/" className="text-white">
+              Ya estas registrado?
+            </Link>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
