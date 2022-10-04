626b98c704a5a8daa5b29f30832a60e545ef068d
Legebrind <moyi88@gmail.com>
2022-10-04 18:08:41 +0000

    RD-23 Done

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-
 group_participation = db.Table("group_participation",
                                db.Column("participant_id", db.Integer, db.ForeignKey(
                                    "user.id"), primary_key=True),
@@ -10,9 +8,12 @@ group_participation = db.Table("group_participation",
                                    'group.id'), primary_key=True)
                                )
 
-friend = db.Table('friend',
-                  db.Column('user_id', db.Integer, db.ForeignKey(
-                      'user.id'), primary_key=True))
+event_participant = db.Table('event_participant',
+                             db.Column("participant_id", db.Integer, db.ForeignKey(
+                                 "user.id"), primary_key=True),
+                             db.Column('event_id', db.Integer, db.ForeignKey(
+                                 'event.id'), primary_key=True)
+                             )
 
 
 class User(db.Model):
@@ -21,9 +22,6 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, default=True)
     # necesito uselist = false porque es una relacion 1 a 1
-    user_data = db.relationship(
-        'User_Data', backref='user', lazy=True, uselist=False)
-
     group_participation = db.relationship('Group', secondary=group_participation, lazy='subquery',
                                           backref=db.backref('users', lazy=True))
 
@@ -61,36 +59,27 @@ class Group(db.Model):
         }
 
 
-class User_Data(db.Model):
+class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=False, nullable=False)
-    last_name = db.Column(db.String(120), unique=False, nullable=False)
-    address = db.Column(db.String(120), unique=False, nullable=False)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
-    profile_picture = db.Column(
-        db.Integer(), db.ForeignKey('image.id'), nullable=True)
-    image_id = db.relationship('Image', backref='user_data', lazy=True)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "last_name": self.last_name,
-            "address": self.address,
-            "user_id": self.user_id,
-            "profile_picture": self.profile_picture
-        }
-
-
-class Image(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.String(), unique=False, nullable=False)
+    start = db.Column(db.String(), unique=False, nullable=False)
+    end = db.Column(db.String(), unique=False, nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey(
-        'user_data.id'), nullable=False)
+        'user.id'), nullable=False)
+    date = db.Column(db.Date(), unique=False, nullable=False)
+    private = db.Column(db.Boolean(), unique=True)
+    slug = db.Column(db.String(), unique=False, nullable=False)
+    description = db.Column(db.String(), unique=False, nullable=False)
+    participants = db.relationship('User', secondary=event_participant, lazy='subquery',
+                                   backref=db.backref('events', lazy=True))
 
     def serialize(self):
         return {
             "id": self.id,
-            "image": self.image,
-            "owner_id": self.owner_id
+            "start": self.start,
+            "end": self.end,
+            "owner_id": self.owner_id,
+            "date": self.date,
+            "private": self.private,
+            "slug": self.slug,
+            "description": self.description
         }
