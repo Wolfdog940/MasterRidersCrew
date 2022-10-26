85d2fad138af6d91d28f385b41744f936bc4bc4f
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-10-26 11:00:01 +0000

    post

--- src/front/js/pages/userHome.js
+++ src/front/js/pages/userHome.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Navbar } from "../component/navbar";
-import SideBar from "../component/sidebar";
+import { ShowPost } from "../component/post/showPosts";
 
 const UserHome = () => {
   return (
     <div className="home">
       <Navbar />
+      <ShowPost />
     </div>
   );
 };
