0e13f06a20d49ba3f09586e4a2ae6ee8ec7d665c
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-11-03 11:49:12 +0000

    ok

--- src/front/js/pages/userHome.js
+++ src/front/js/pages/userHome.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Navbar } from "../component/navbar";
 import { ShowPost } from "../component/post/showPosts";
-import { Picker } from "../component/picker.jsx";
 
 const UserHome = () => {
   return (
     <div className="home">
       <Navbar />
-      {/* <ShowPost /> */}
-      <Picker />
+      <ShowPost />
     </div>
   );
 };
