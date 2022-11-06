7e583e70fb9abed412108ed4028eee3bafe6d522
Legebrind <76969241+Legebrind@users.noreply.github.com>
2022-11-06 23:05:05 +0100

    Update routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -249,7 +249,7 @@ def add_event():
         date=date,
         slug=slug,
         description=description,
-        map="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97173.78941670264!2d-3.7495758376144654!3d40.4380638218829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422997800a3c81%3A0xc436dec1618c2269!2sMadrid!5e0!3m2!1sen!2ses!4v1667475409979!5m2!1sen!2ses",
+        map="https://www.google.es/maps/dir///@39.6202832,-2.6060788,9z/data=!4m2!4m1!3e0?hl=es",
         origin_lon=origin_lon,
         origin_lat=origin_lat,
         destination_lon=destination_lon,
