f7dbfcc8aeaeedc5ff9cf100397d3a709780bbd9
RodriMaldonadoMedina <91276299+RodriMaldonadoMedina@users.noreply.github.com>
2022-11-10 23:36:34 +0100

    Update showEvent.jsx

--- src/front/js/component/Events/showEvent.jsx
+++ src/front/js/component/Events/showEvent.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../../store/appContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { Navbar } from "../navbar";
+import "../../../styles/showEvent.css";
 
 const ShowEvent = () => {
   const { store, actions } = useContext(Context);
@@ -61,49 +62,61 @@ const ShowEvent = () => {
     <div>
       <Navbar />
       <div className="event-container event-scroll">
-        <iframe src={event.map} style={{"width":"80%", "max-width":"700px", "height":"500px"}}></iframe>
+        <iframe src={event.map} className="mapaShowEvent"></iframe>
         <div className="event-post">
-          <label htmlFor="name">Nombre del evento</label>
-          <h5 id="name" className="card-title">
-            {event.name}
-          </h5>
-          <label htmlFor="date">Fecha</label>
-          <p id="date" className="postText">
-            {event.date} a las {event.hours}:{event.minutes}
-          </p>
-          <label htmlFor="start">Ciudad de inicio</label>
-          <p id="start" className="postText">
-            {event.start}
-          </p>
-          <label htmlFor="end">Ciudad de destino</label>
-          <p id="end" className="postText">
-            {event.end}
-          </p>
-          <label htmlFor="description">Descripcion</label>
-          <p id="description" className="postText">
-            {event.description}
-          </p>
-          <input
-            id="mapInput"
-            placeholder="Pega aqui la ruta de google maps"
-          ></input>
-
-          <button onClick={sendMap} className="btn btn-primary">
-            Enviar mapa
-          </button>
-          {eventParticipation ? (
-            <button onClick={unsubscribe} className="btn btn-primary ms-5">
-              Borrarse del evento
-            </button>
-          ) : (
-            <button onClick={subscribe} className="btn btn-primary ms-5">
-              Inscribirse en el evento
-            </button>
-          )}
+          <div className="divShowEvent">
+            <label htmlFor="name" className="text-secondary labelShowEvent">Nombre del evento</label>
+            <h5 id="name" className="card-title">
+              {event.name}
+            </h5>
+          </div>
+          <div className="divShowEvent">
+            <label htmlFor="date" className="text-secondary labelShowEvent">Fecha</label>
+            <p id="date">
+              {event.date} a las {event.hours}:{event.minutes}
+            </p>
+          </div>
+          <div className="divShowEvent">
+            <label htmlFor="start" className="text-secondary labelShowEvent">Ciudad de inicio</label>
+            <p id="start">
+              {event.start}
+            </p>
+          </div>
+          <div className="divShowEvent">
+            <label htmlFor="end" className="text-secondary labelShowEvent">Ciudad de destino</label>
+            <p id="end">
+              {event.end}
+            </p>
+          </div>
+          <div className="divShowEvent">
+            <label htmlFor="description" className="text-secondary labelShowEvent">Descripcion</label>
+            <p id="description">
+              {event.description}
+            </p>
+          </div>
+          <div className="showFooter d-flex flex-column">
+            <div className="d-flex">
+              <input
+                id="mapInput"
+                placeholder="Pega aqui la ruta de google maps"
+                className="showFooterInput"
+              ></input>
+              <button onClick={sendMap} className="btn btn-outline-secondary enterBtn showFooterBtnAddMap">
+                Enviar mapa
+              </button>
+            </div>
+            {eventParticipation ? (
+              <button onClick={unsubscribe} className="btn btn-outline-warning mt-2 leaveBtn showFooterBtnParticipation">
+                Borrarse del evento
+              </button>
+            ) : (
+              <button onClick={subscribe} className="btn btn-outline-success mt-2 enterBtn showFooterBtnParticipation">
+                Inscribirse en el evento
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
-export default ShowEvent;
