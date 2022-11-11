87781f7bc5e2694b93e942a32cce4a052e3ed673
Antonio Moyano Molina <76969241+Legebrind@users.noreply.github.com>
2022-11-11 23:47:24 +0100

    Update showEvent.jsx

--- src/front/js/component/Events/showEvent.jsx
+++ src/front/js/component/Events/showEvent.jsx
@@ -9,6 +9,7 @@ const ShowEvent = () => {
   const [event, setEvent] = useState({});
   const params = useParams();
   const [eventParticipation, setEventParticipation] = useState(true);
+  const [eventParticipants, setEventParticipants] = useState(0);
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -19,21 +20,43 @@ const ShowEvent = () => {
     setEventParticipation(actions.searchEvent(params.eventId));
   }, []);
 
-  const getEventAndList = async () => {
-    await actions.getEvent(params.eventId);
-    await actions.listEvents();
-  };
-
   useEffect(() => {
     setEvent(store.event);
     setEventParticipation(actions.searchEvent(params.eventId));
+    getEventParticipants();
   }, [store.event]);
 
   useEffect(() => {
     if (store.userEventParticipation.length > 0) {
       setEventParticipation(actions.searchEvent(params.eventId));
+      getEventParticipants();
     }
   }, [store.eventParticipation]);
+
+  const getEventParticipants = async () => {
+    const opts = {
+      headers: {
+        Authorization: "Bearer " + localStorage.token,
+      },
+    };
+    try {
+      const resp = await fetch(
+        process.env.BACKEND_URL + "/api/listParticipants/" + params.eventId,
+        opts
+      );
+      const data = await resp.json();
+      setEventParticipants(data.participantsAmount);
+      return data;
+    } catch (error) {
+      console.error("There has been an error retrieving data");
+    }
+  };
+
+  const getEventAndList = async () => {
+    await actions.getEvent(params.eventId);
+    await actions.listEvents();
+    await getEventParticipants();
+  };
 
   const sendMap = (e) => {
     e.preventDefault();
@@ -49,6 +72,7 @@ const ShowEvent = () => {
     await actions.joinEvent(id);
     await actions.listEvents();
     setEventParticipation(actions.searchEvent(params.eventId));
+    await getEventParticipants();
   };
 
   const unsubscribe = async (e) => {
@@ -57,6 +81,7 @@ const ShowEvent = () => {
     await actions.unsubscribeEvent(id);
     await actions.listEvents();
     setEventParticipation(actions.searchEvent(params.eventId));
+    await getEventParticipants();
   };
   return (
     <div>
@@ -65,34 +90,50 @@ const ShowEvent = () => {
         <iframe src={event.map} className="mapaShowEvent"></iframe>
         <div className="event-post">
           <div className="divShowEvent">
-            <label htmlFor="name" className="text-secondary labelShowEvent">Nombre del evento</label>
+            <label htmlFor="name" className="text-secondary labelShowEvent">
+              Nombre del evento
+            </label>
             <h5 id="name" className="card-title">
               {event.name}
             </h5>
           </div>
           <div className="divShowEvent">
-            <label htmlFor="date" className="text-secondary labelShowEvent">Fecha</label>
+            <label htmlFor="date" className="text-secondary labelShowEvent">
+              Fecha
+            </label>
             <p id="date">
               {event.date} a las {event.hours}:{event.minutes}
             </p>
           </div>
           <div className="divShowEvent">
-            <label htmlFor="start" className="text-secondary labelShowEvent">Ciudad de inicio</label>
-            <p id="start">
-              {event.start}
-            </p>
+            <label htmlFor="start" className="text-secondary labelShowEvent">
+              Ciudad de inicio
+            </label>
+            <p id="start">{event.start}</p>
           </div>
           <div className="divShowEvent">
-            <label htmlFor="end" className="text-secondary labelShowEvent">Ciudad de destino</label>
-            <p id="end">
-              {event.end}
-            </p>
+            <label htmlFor="end" className="text-secondary labelShowEvent">
+              Ciudad de destino
+            </label>
+            <p id="end">{event.end}</p>
           </div>
           <div className="divShowEvent">
-            <label htmlFor="description" className="text-secondary labelShowEvent">Descripcion</label>
-            <p id="description">
-              {event.description}
-            </p>
+            <label
+              htmlFor="description"
+              className="text-secondary labelShowEvent"
+            >
+              Descripcion
+            </label>
+            <p id="description">{event.description}</p>
+          </div>
+          <div className="divShowEvent">
+            <label
+              htmlFor="amountParticipants"
+              className="text-secondary labelShowEvent"
+            >
+              Cantidad de participantes
+            </label>
+            <p id="amountParticipants">{eventParticipants}</p>
           </div>
           <div className="showFooter d-flex flex-column">
             <div className="d-flex">
@@ -101,16 +142,25 @@ const ShowEvent = () => {
                 placeholder="Pega aqui la ruta de google maps"
                 className="showFooterInput"
               ></input>
-              <button onClick={sendMap} className="btn btn-outline-secondary enterBtn showFooterBtnAddMap">
+              <button
+                onClick={sendMap}
+                className="btn btn-outline-secondary enterBtn showFooterBtnAddMap"
+              >
                 Enviar mapa
               </button>
             </div>
             {eventParticipation ? (
-              <button onClick={unsubscribe} className="btn btn-outline-warning mt-2 leaveBtn showFooterBtnParticipation">
+              <button
+                onClick={unsubscribe}
+                className="btn btn-outline-warning mt-2 leaveBtn showFooterBtnParticipation"
+              >
                 Borrarse del evento
               </button>
             ) : (
-              <button onClick={subscribe} className="btn btn-outline-success mt-2 enterBtn showFooterBtnParticipation">
+              <button
+                onClick={subscribe}
+                className="btn btn-outline-success mt-2 enterBtn showFooterBtnParticipation"
+              >
                 Inscribirse en el evento
               </button>
             )}
