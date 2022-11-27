64c2ec12cb78752b93a4d55071573b7a98d0b7a8
RodriMaldonadoMedina <maldonadomedinarodrigo@gmail.com>
2022-11-27 17:14:25 +0000

    se añadio la parte responsive a las filas y columnas del userHome

--- src/front/js/pages/userHome.js
+++ src/front/js/pages/userHome.js
@@ -19,12 +19,19 @@ const UserHome = () => {
     <div className="home ">
       <Navbar />
       <div className="container">
-        <div className="row finder-container">
-            <div className=" rigth-container">
+        <div className="row bigRow">
+            <div className="col-3 g-0">
               <Friends />
               <MyFriends />
             </div>
-            <ShowPost />
+            <div className="col-9">
+              <ShowPost />
+            </div>
+        </div>
+        <div className="smallRow">
+          <Friends />
+          <MyFriends />
+          <ShowPost />
         </div>
       </div>
     </div>
